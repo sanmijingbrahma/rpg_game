d4d76a5b4a98a7ce245278a3a2ecd71d1dbfaf00
sanmijingbrahma <bsanmijing@gmail.com>
2025-08-20 14:09:29 +0530

    Basic game loop and drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,48 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Text.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
-#include<iostream>
+#include <string>
 
 
 int main(){
-
+    //Create Window
     sf::RenderWindow window(sf::VideoMode(800,600),"RPG Game 1");
 
+    
+    //Create Rectangle
+    sf::RectangleShape map(sf::Vector2f(400.f,300.f));
+    map.setPosition(200.f,150.f);
+    map.setFillColor(sf::Color::Green);
+   
+
+    sf::Font font;
+    if(!font.loadFromFile("../assets/fonts/dragon.ttf")){
+        return 1;
+    }
+
+    sf::Text fpsText;
+    fpsText.setFont(font);
+    fpsText.setCharacterSize(20);
+    fpsText.setFillColor(sf::Color::White);
+    fpsText.setPosition(10.f,10.f);
+
+
+    sf::Clock clock;
+    float lastTime = 0.f;
+
+
+
+ 
+
+
+
+    //Game loop
     while(window.isOpen()){
+        //handle Events
         sf::Event event;
         while(window.pollEvent(event)){
             if(event.type ==sf::Event::Closed){ 
@@ -17,9 +50,21 @@ int main(){
             }
             
         }
-
-
+        
+        // Update
+        float currentTime = clock.getElapsedTime().asSeconds();
+        float fps = 1.f/(currentTime - lastTime);
+        lastTime = currentTime;
+        fpsText.setString("FPS : "+std::to_string(static_cast<int>(fps)));
+        
+        //Clear window
         window.clear();
+
+        //Draw
+        window.draw(map);
+        window.draw(fpsText);
+
+        //display
         window.display();
     }
 
