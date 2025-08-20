85a099524cd03fd4f86c0cbb6bfb939e2e7b3e78
sanmijingbrahma <bsanmijing@gmail.com>
2025-08-20 13:28:13 +0530

    SFML window set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include<iostream>
 
 
 int main(){
+
+    sf::RenderWindow window(sf::VideoMode(800,600),"RPG Game 1");
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type ==sf::Event::Closed){ 
+                window.close();
+            }
+            
+        }
+
+
+        window.clear();
+        window.display();
+    }
+
+
     
-    std::cout<<"Hello World!"<<std::endl;
 
     return 0;
 }
